e10a07f0dbcd130ca866c37dd3182b8d089c4e1a
Sonia V Mathew <sonisvm@gmail.com>
2015-07-23 19:30:32 +0530

    testing

---
+++ MyUnitTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import BestScrabbleWord;
+import org.junit.Assert.*;
+
+public class MyUnitTest {
+
+    @Test
+    public void testGetTheSameObject() {
+        BestScrabbleWord BestScrabbleWordTest = new BestScrabbleWord();
+    }
+}
